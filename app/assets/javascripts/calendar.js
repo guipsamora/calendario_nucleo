2bf015cc4a0cc344748d002ca3134a62d030f0e0
guipsamora <gui.psamora@gmail.com>
2015-12-27 01:56:42 -0200

    adicionada lista de pilares e alteracao de cor conforme pilar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,9 +1,23 @@
 $('document').ready(function() {
   $('#calendar').fullCalendar({
     events: '/events.json', 
-    eventRender: function(event, element) { 
+    eventRender: function(event, element) {
+      console.log(element)
       element.find('.fc-title').append("<br/>" + event.nome + " " + event.pilar);
       element.find('.fc-title').append("<br/>" + event.descricao);
+      switch (event.pilar) {
+        case 'Redes':
+          element.addClass("redes");
+          break
+        case 'Mercado':
+          element.addClass("mercado");
+          break
+        case 'Propósito':
+          element.addClass("proposito");
+          break    
+        default:
+          break
+      }
     },
     theme: true,
     lang: 'pt-br',
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   validates :nome, :pilar, :descricao, :local, presence: true
+
+  def self.pilares
+     [['Liderança','Liderança'], ['Redes', 'Redes'], ['Mercado', 'Mercado'], ['Propósito', 'Propósito']]
+  end
+
 end
